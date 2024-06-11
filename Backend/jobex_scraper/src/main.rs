7cf941be9a6707f8f1c36f972f7293746c84afc4
Caleb Archer <calebandcorey@gmail.com>
2024-06-12 01:28:57 +1200

    Add sleeping to search loop

--- Backend/jobex_scraper/src/main.rs
+++ Backend/jobex_scraper/src/main.rs
@@ -1,8 +1,11 @@
 use std::error::Error;
 use std::sync::Arc;
+use std::time::Duration;
 use futures::StreamExt;
 use chromiumoxide::{Browser, BrowserConfig, Page};
 use chromiumoxide::cdp::browser_protocol::fetch::{ContinueRequestParams, EventRequestPaused};
+use tokio::join;
+use tokio::time::sleep;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -34,14 +37,19 @@ async fn main() -> Result<(), Box<dyn Error>> {
         "office administrator",
         "human resources",
         "recruiter"
-    ].map(|job| job.replace(' ', "+"));
+    ];
     
     for job in jobs {
-        let job_count = get_job_count(&page, &job).await;
+        let job_query_string = job.replace(' ', "+");
+        
+        let (job_count, _) = join!(
+            get_job_count(&page, &job_query_string),
+            sleep(Duration::from_millis(500))
+        );
         
         match job_count {
-            Ok(Some(count)) => println!("Job count: {}", count),
-            _ => println!("Job count: None found")
+            Ok(Some(count)) => println!("Job count for {}: {}", job, count),
+            _ => println!("Job count for {}: None found", job)
         }
     }
 
@@ -53,7 +61,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
 }
 
 async fn get_job_count(page: &Page, job_title: &str) -> Result<Option<u32>, Box<dyn Error>> {
-    page.goto("https://nz.indeed.com/jobs?q={}").await?;
+    page.goto(format!("https://nz.indeed.com/jobs?q={}", job_title)).await?;
     page.wait_for_navigation_response().await?;
 
     let job_count_div = page.find_element("div.jobsearch-JobCountAndSortPane-jobCount").await?;
