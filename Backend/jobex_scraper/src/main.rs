0c6b2a51e5a1ac0a48f59d82ac76df1d23f9f0b8
Caleb Archer <calebandcorey@gmail.com>
2024-06-12 01:12:13 +1200

    Add basic search testing functionality

--- Backend/jobex_scraper/src/main.rs
+++ Backend/jobex_scraper/src/main.rs
@@ -1,10 +1,15 @@
 use std::error::Error;
+use std::sync::Arc;
 use futures::StreamExt;
-use chromiumoxide::{Browser, BrowserConfig};
+use chromiumoxide::{Browser, BrowserConfig, Page};
+use chromiumoxide::cdp::browser_protocol::fetch::{ContinueRequestParams, EventRequestPaused};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let browser_config = BrowserConfig::builder().build()?;
+    let browser_config = BrowserConfig::builder()
+        .window_size(1920, 1080)
+        .build()?;
+
     let (mut browser, mut handler) = Browser::launch(browser_config).await?;
 
     let handle = tokio::task::spawn(async move {
@@ -15,21 +20,56 @@ async fn main() -> Result<(), Box<dyn Error>> {
         }
     });
 
-    let now = std::time::Instant::now();
-
-    let page = browser.new_page("https://www.rust-lang.org").await?;
-    let page2 = browser.new_page("https://www.wikipedia.org").await?;
-    let user_agent = page.user_agent().await?;
-    let content_size = page2.content().await?.len();
-
-    println!("User agent: {}", user_agent);
-    println!("Wikipedia content size: {}", content_size);
-    println!("Elapsed: {:?}", now.elapsed());
+    let page = Arc::new(browser.new_page("about:blank").await?);
+    page.enable_stealth_mode_with_agent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Safari/605.1.15").await?;
+    
+    let jobs = [
+        "software engineer", 
+        "software developer", 
+        "civil engineer",
+        "web developer",
+        "accountant",
+        "financial adviser",
+        "policy advisor",
+        "office administrator",
+        "human resources",
+        "recruiter"
+    ].map(|job| job.replace(' ', "+"));
+    
+    for job in jobs {
+        let job_count = get_job_count(&page, &job).await;
+        
+        match job_count {
+            Ok(Some(count)) => println!("Job count: {}", count),
+            _ => println!("Job count: None found")
+        }
+    }
 
     browser.close().await?;
     browser.wait().await?;
-
     handle.await?;
 
     Ok(())
+}
+
+async fn get_job_count(page: &Page, job_title: &str) -> Result<Option<u32>, Box<dyn Error>> {
+    page.goto("https://nz.indeed.com/jobs?q={}").await?;
+    page.wait_for_navigation_response().await?;
+
+    let job_count_div = page.find_element("div.jobsearch-JobCountAndSortPane-jobCount").await?;
+    let job_count_span = job_count_div.find_element("span").await?;
+    let count_text = job_count_span.inner_html().await?;
+    
+    let Some(count_text) = count_text else {
+        return Ok(None);
+    };
+
+    let count = count_text
+        .split_whitespace()
+        .next()
+        .ok_or("No job count text found")?
+        .trim()
+        .parse::<u32>()?;
+    
+    Ok(Some(count))
 }
